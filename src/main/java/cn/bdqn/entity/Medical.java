ec19c1acb174cd8504f7a2f974def103b6818db0
941339912 <panaglei0677@yeah.net>
2019-07-25 20:17:49 +0800

    1.4.5

---
+++ src/main/java/cn/bdqn/entity/Medical.java
@@ -0,0 +1,27 @@
+package cn.bdqn.entity;
+
+
+public class Medical {
+
+  private long id;
+  private String name;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
