bfa5b89cbf005f615b2bb6c03be1b1852e20769b
941339912 <panaglei0677@yeah.net>
2019-07-27 20:43:04 +0800

    查询所有医生--dao层、service层

--- src/main/java/cn/bdqn/service/doctor/DoctorService.java
+++ src/main/java/cn/bdqn/service/doctor/DoctorService.java
@@ -1,4 +1,14 @@
 package cn.bdqn.service.doctor;
 
+import cn.bdqn.entity.Doctor;
+
+import java.util.List;
+
 public interface DoctorService {
+    /**
+     * 查询所有医生，模糊查询，拼音查询
+     *
+     * @return
+     */
+    public List<Doctor> getQueryDoctor(String dname, String dpinyin);
 }
--- src/main/java/cn/bdqn/service/doctor/DoctorServiceImpl.java
+++ src/main/java/cn/bdqn/service/doctor/DoctorServiceImpl.java
@@ -1,4 +1,20 @@
 package cn.bdqn.service.doctor;
 
+import cn.bdqn.dao.DoctorMapper;
+import cn.bdqn.entity.Doctor;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
 public class DoctorServiceImpl implements DoctorService {
+
+    @Resource
+    private DoctorMapper doctorMapper;
+
+    @Override
+    public List<Doctor> getQueryDoctor(String dname, String dpinyin) {
+        return doctorMapper.findQueryDoctor(dname, dpinyin);
+    }
 }
