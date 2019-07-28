a5e27cd92779d9cf5ff7b5946800a8971ab9fc3f
941339912 <panaglei0677@yeah.net>
2019-07-28 17:28:07 +0800

    科室查询--医生查询

--- src/main/resources/static/js/doctor.js
+++ src/main/resources/static/js/doctor.js
@@ -39,9 +39,44 @@ $(function () {
     });
 
     var pyli = $(".IndexWords ul li");
+    var ziText ="";
     pyli.click(function () {
         // alert($(this).text());
-        $.post("queryDoc",{dpinyin:$(this).text()},function (data) {
+        if($(this).text()==""||$(this).text()==null){
+            ziText=$(this).text();
+        }else {
+            ziText=ziText+$(this).text();
+        }
+        $("#zimuId").val(ziText.toLowerCase());
+        $.post("queryDoc",{dpinyin:$("#zimuId").val()},function (data) {
+            var doctorList = data.doctorList;
+            var $ul = $(".ul_zimu");
+            $ul.html("");
+            if(doctorList.length==0){
+                $ul.html("<h1>不存在该医生</h1>")
+            }
+            for (var i=0;i<doctorList.length;i++){
+                $li = $("<li><a href=\"ys\" title=\""+doctorList[i].dName+"\" target=\"_blank\" class=\"img\"><img alt=\""+doctorList[i].dName+"\" title=\""+doctorList[i].dName+"\"\n" +
+                    "src=\"doctorImages/"+doctorList[i].photopath+"\" width=\"120\" height=\"162\"/></a>\n" +
+                    "<a href=\"ys\" title=\""+doctorList[i].dName+"\" target=\"_blank\" class=\"name\">"+doctorList[i].dName+"</a>\n" +
+                    "<a href=\"keshi\" title=\""+doctorList[i].graduate+"\" target=\"_blank\" class=\"office\">"+doctorList[i].graduate+"</a>\n" +
+                    "<a href=\"ys\" title=\""+doctorList[i].postion+"\" target=\"_blank\" class=\"post\">"+doctorList[i].postion+"</a>\n" +
+                    "<a href=\"ys\" title=\"详细\" target=\"_blank\" class=\"btn more\">详细</a>\n" +
+                    "<a href=\"#\" target=\"_blank\" class=\"btn date\">预约</a></li>");
+                $ul.append($li);
+            }
+        },"JSON");
+    });
+
+    $("#zimuDel").click(function () {
+        ziText = $("#zimuId").val();
+        ziText=ziText.substring(0,ziText.length-1);
+        $("#zimuId").val(ziText);
+        if($("#zimuId").val()==""){
+            $(".ul_zimu").html("");
+            return;
+        }
+        $.post("queryDoc",{dpinyin:$("#zimuId").val()},function (data) {
             var doctorList = data.doctorList;
             var $ul = $(".ul_zimu");
             $ul.html("");
--- src/main/resources/static/js/koffice.js
+++ src/main/resources/static/js/koffice.js
@@ -1,5 +1,10 @@
 $(function () {
 
+    // //单击科室简介
+    // $("#officejianjie").click(function () {
+    //     alert("aaaa");
+    //     $(".doctorList").load("officejianjie.html .doctorRight");
+    // });
     //单击科室简介
     $("#officejianjie").click(function () {
         alert("aaaa");
@@ -15,11 +20,11 @@ function showEmphasisKoffice() {
         var $ul = $("#officeEmphasis");
         for( var i = 0;i<data.length;i++ ){
             var $li = $("<li><i class=\"ico01\"></i>\n" +
-                "                <div class=\"img animated\"><img alt=\'"+data[i].kName+"'\" src=\"pic/pic_007.png\" width=\"80\" height=\"80\"/></div>\n" +
+                "                <div class=\"img animated\"><img alt=\""+data[i].kName+"\" src=\"pic/pic_007.png\" width=\"80\" height=\"80\"/></div>\n" +
                 "                <a href=\"kofficeById?id="+data[i].id+"\" title=\"'"+data[i].kName+"'\" class=\"office\">"+data[i].kName+"</a>\n" +
                 "                <p style=\"display:-webkit-box; -webkit-line-clamp:3; -webkit-box-orient:vertical; overflow:hidden;\">"+data[i].resume+"</p>\n" +
-                "                <a href=\"kofficeById?id="+data[i].id+"\" title=\"'"+data[i].kName+"'\" class=\"btn more\" >查看详细</a>\n" +
-                "                <a href=\"doctorByKId?id="+data[i].id+"\" title=\"'"+data[i].kName+"'\" class=\"btn doctor\">科室专家</a></li>\n");
+                "                <a href=\"kofficeById?id="+data[i].id+"\" title=\""+data[i].kName+"\" class=\"btn more\" >查看详细</a>\n" +
+                "                <a href=\"doctorByKId?id="+data[i].id+"\" title=\""+data[i].kName+"\" class=\"btn doctor\">科室专家</a></li>\n");
             $ul.append($li);
         }
     },"json");
